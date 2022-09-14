2687c336a0616fde42eab5af371d8abd48fcdc30
YunJaem06 <yunjae0616@gmail.com>
2022-09-14 18:22:56 +0900

    캡스톤 회원가입 레이아웃 교체

--- app/src/main/java/com/hairmunk/app/ui/AuthFragment.kt
+++ app/src/main/java/com/hairmunk/app/ui/AuthFragment.kt
@@ -13,33 +13,5 @@ import com.hairmunk.app.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
 
-    private lateinit var Auth: FirebaseAuth
-    private var _binding: FragmentAuthBinding? = null
-    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        Auth = Firebase.auth
-        _binding = FragmentAuthBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-    // 로그아웃 구현
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.btnLogout.setOnClickListener {
-            val intent = Intent(context, LoginActivity::class.java)
-            startActivity(intent)
-            Auth.signOut()
-        }
-        binding.tvAuth.text = "안녕하세요 ${Auth.currentUser?.email} 님"
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
--- app/src/main/java/com/hairmunk/app/ui/LoginActivity.kt
+++ app/src/main/java/com/hairmunk/app/ui/LoginActivity.kt
@@ -4,49 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.hairmunk.app.R
 import com.hairmunk.app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private var auth: FirebaseAuth? = null
+
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var otherDialog: BottomSheetDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        auth = Firebase.auth
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        // 다른 로그인 바텀시트
+        otherLogin()
 
-        binding.btnLoginJoin.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnLoginOk.setOnClickListener {
-            val email = binding.edLoginEmail.text.toString()
-            val password = binding.edLoginPassword.text.toString()
-            auth!!.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
-                        moveMainPage(auth?.currentUser)
-                    } else {
-                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
-                    }
-                }
-        }
     }
 
-    public override fun onStart() {
-        super.onStart()
-        moveMainPage(auth?.currentUser)
+    private fun otherLogin() {
+
+        val otherView = layoutInflater.inflate(R.layout.dialog_login_other_sheet, null)
+        otherDialog = BottomSheetDialog(this)
+        otherDialog.setContentView(otherView)
+
+        binding.tvLoginOther.setOnClickListener {
+            otherDialog.show()
+        }
+
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if( user != null){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-    }
 }
