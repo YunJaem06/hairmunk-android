76e35d2fd09a99f3e716563acfce29e43f8c28a0
YunJaem06 <yunjae0616@gmail.com>
2022-10-12 21:39:31 +0900

    kakao map에 필요한 준비작업

---
+++ app/src/main/java/com/hairmunk/app/ui/map/MapFragment.kt
@@ -0,0 +1,22 @@
+package com.hairmunk.app.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hairmunk.app.databinding.FragmentMapBinding
+
+class MapFragment : Fragment() {
+
+    private lateinit var binding: FragmentMapBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
