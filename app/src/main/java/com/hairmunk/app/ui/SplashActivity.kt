b605470407fd43154ea389592ba2cb55c09144b5
yunjaem06 <yunjae0616@gmail.com>
2023-03-08 18:32:30 +0900

    Lottie splash 적용

--- app/src/main/java/com/hairmunk/app/ui/SplashActivity.kt
+++ app/src/main/java/com/hairmunk/app/ui/SplashActivity.kt
@@ -2,15 +2,25 @@ package com.hairmunk.app.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.hairmunk.app.databinding.ActivitySplashBinding
 import com.hairmunk.app.ui.login.LoginActivity
 
-class SplashActivity: AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
